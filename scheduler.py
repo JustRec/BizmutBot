b1cb48a5595429cfd3663cb2cdf9550e74755b16
JustRec <mmmollaoglumehmet@gmail.com>
2022-03-06 14:53:07 +0300

    Update scheduler.py
    add simple scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -2,12 +2,6 @@ import schedule
 import time
 from threading import Thread
 
-def __IsItNotifTime():
-    sys_time = int(time.ctime(time.time())[11:13] + time.ctime(time.time())[14:16] ) #Hour and minute value in local time
-    print(f"Notification check at: {sys_time}")
-
-    return sys_time != 1000 and sys_time != 1300 #Remove 2 time to make total notif count to 15
-
 def __Schedule_Checker():
     while True:
         schedule.run_pending()
@@ -28,17 +22,12 @@ def __Freq_Flag():
 
 
 def __Job():
-    if __IsItNotifTime():
-        print("its working")
-
     updater.bot.send_message(chat_id=1467840078, text="I'm a bot, please talk to me!")
 
 
 def __Schedule_Starter(): #Start the scheduler at .00 or .30
-    while int(time.ctime(time.time())[14:16]) != 00 and int(time.ctime(time.time())[14:16]) != 30:
-        time.sleep(1)
     
-    schedule.every(5).seconds.do(__Job)
+    schedule.every().day.at("18:00").do(__Job)
     Thread(target=__Schedule_Checker).start()
 
 def Scheduler(Updater,Cursor):
